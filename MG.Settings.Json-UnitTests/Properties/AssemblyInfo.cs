fe55c181cc7ca5de01b74e8bdcb40b3882444e83
Mike Garvey <mike.garvey@yevrag35.com>
2019-11-19 17:32:32 -0500

    Changes

--- MG.Settings.Json-UnitTests/Properties/AssemblyInfo.cs
+++ MG.Settings.Json-UnitTests/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("MG.Settings.Json-UnitTests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Unit Tests for MG.Settings.Json")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Yevrag35, LLC.")]
 [assembly: AssemblyProduct("MG.Settings.Json-UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2019 Yevrag35, LLC.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("efe7d102-f7cc-4c36-b4e3-e4cd569ab64a")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.1")]
+[assembly: AssemblyFileVersion("1.1.1")]
