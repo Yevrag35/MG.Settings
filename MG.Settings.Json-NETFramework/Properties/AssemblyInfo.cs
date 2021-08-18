affad52bc042fdc122b6fa9ce66345eac9131217
Mike Garvey <mike.garvey@yevrag35.com>
2021-08-18 08:48:07 -0400

    v2.2.0

---
+++ MG.Settings.Json-NETFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MG.Settings.Json - .NET Framework")]
+[assembly: AssemblyDescription("A library for maintaining, editing, and saving configuration settings stored in JSON text files for use with WPF applications.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Yevrag35, LLC.")]
+[assembly: AssemblyProduct("MG.Settings.Json - .NET Framework")]
+[assembly: AssemblyCopyright("Copyright © 2018-2021 Yevrag35, LLC.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("adaa8d1b-344d-46a4-a3bf-60b9bf6059c5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.2.0")]
+[assembly: AssemblyFileVersion("2.2.0")]
